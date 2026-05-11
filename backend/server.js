3aecdca9f74d0822affa30a3f3a913b384cfe6e9
pradeepkum1 <pradeepk48842@gmail.com>
2026-05-11 11:46:29 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import { createServer } from "http"
+import {Server } from "socket.io"
+import { YSocketIO } from "y-socket.io/dist/server"
+import dotenv from "dotenv"
+
+
+const app = express()
+dotenv.config();
+app.use(express.static("public"))
+
+const PORT = process.env.PORT || 4000;
+
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods : [ "GET", "POST"]
+    }
+})
+
+const ySocketIO = new YSocketIO(io)
+ySocketIO.initialize()
+
+
+
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        message: "OK",
+        success: true
+    })
+})
+
+
+httpServer.listen(PORT, () => {
+    console.log("Server is running on port 3000")
+})
---
+++ dockerfile
@@ -0,0 +1,31 @@
+# Container banane ke liye Node.js version 20 ka lightweight Alpine Linux image use karo
+# FROM node:20-alpine
+
+# COPY ./backend .
+
+# RUN npm install
+
+# CMD ["node", "server.js"]
+
+FROM node:20-alpine as frontend-builder
+
+COPY ./frontend /app
+WORKDIR /app
+
+RUN npm install
+
+# make dist folder
+RUN npm run build
+
+# backend
+
+FROM node:20-alpine
+
+COPY ./backend /app
+WORKDIR /app
+
+RUN npm install
+
+COPY --from=frontend-builder /app/dist /app/public
+
+CMD ["node", "server.js"]
